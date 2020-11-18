39e11b2ee57fbeb7332bb54db88093e9e84919a1
Kevinli296 <kevinli296@outlook.com>
2020-11-18 08:10:10 +0000

    Cleaned up code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,36 +1,7 @@
-// Short-hand: $(() => {});
-
 $(document).ready(() => {
-  // console.log(jQuery);
 
-  // assign jquery variable for the textbox
   const $tweetText = $('#tweet-text');
   
-  // registering focus for when entering textbox
-  // *remember using FAT ARROW will not work when referencing "this"
-  // $tweetText.focus (function() {
-  //   console.log('Clicked into textbox')
-  //   // console.log(this);
-  // });
-
-  // registering blur for when leaving textbox
-  // $tweetText.blur (function() {
-  //   console.log('Clicked out of textbox');
-  //   // console.log(this);
-  // });
-
-  // registering keypress event handler within textbox 
-  // for when there is input
-  // $tweetText.keypress (() => {
-  //   console.log('Key was pressed');
-  // });
-
-  // registering keyup handler when key is released
-  // $tweetText.keyup (() => {
-  // console.log('Key was released');
-  // });
-
-  // registering keydown handler when key is pressed down
   $tweetText.on('input', function() {
     const value = 140 - $(this).val().length;
     const counter = $(this).parent().find('#counterNumber');
@@ -42,17 +13,8 @@ $(document).ready(() => {
     }
     
   });
-
-
-
-  // registering input handler for when there is an input
-  // $tweetValue.input();
-
-  $tweetText.change (() => {
-    console.log('Change');
-  });
-
-
 });
+
+
 
 
--- public/scripts/hover-state.js
+++ public/scripts/hover-state.js
@@ -1,16 +1,11 @@
-// Short-hand: $(() => {});
-
 $(document).ready(() => {
-  // console.log(jQuery);
 
-  // assign jquery variable for the textbox
   const $existingTweet = $('.tweet');
 
   $existingTweet.on('mouseenter', function () {
     const handle = $(this).parent().find('.tweet-handle');
     handle.removeClass('hover-state');
     $existingTweet.addClass('box-shadow');
-
   });
 
   $existingTweet.on('mouseleave', function() {
@@ -19,15 +14,4 @@ $(document).ready(() => {
     $existingTweet.removeClass('box-shadow')
   });
 
-  // $tweetText.on('input', function () {
-  //   const value = 140 - $(this).val().length;
-  //   const counter = $(this).parent().find('#counterNumber');
-  //   counter.text(value);
-  //   if (value < 0) {
-  //     counter.addClass('red-color');
-  //   } else {
-  //     counter.removeClass('red-color');
-  //   }
-
-  // });
 });
