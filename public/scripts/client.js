b9b058b20c0e37bf73114e56c15d707be48ede25
Kevinli296 <kevinli296@outlook.com>
2020-11-19 01:46:11 +0000

    Added ajax GET request to fetch tweets and a POST request to prevent form submission from reloading the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,22 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready (() => {
-
-    // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
- }
-
- const data = [
+// DATA -------------------------------------------------
+const data = [
   {
     "user": {
       "name": "Newton",
@@ -36,7 +22,8 @@ const tweetData = {
     "user": {
       "name": "Descartes",
       "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
+      "handle": "@rd"
+    },
     "content": {
       "text": "Je pense , donc je suis"
     },
@@ -44,8 +31,8 @@ const tweetData = {
   }
 ]
 
- const renderTweets = function(tweets) {
-   let result = {};
+const renderTweets = function (tweets) {
+  let result = {};
   for (const tweet of tweets) {
     result = createTweetElement(tweet);
     $('#tweets-container').append(result);
@@ -54,28 +41,86 @@ const tweetData = {
   return result;
 }
 
-const createTweetElement = function(tweet) {
+// FUNCTIONS ----------------------------------------------
+const createTweetElement = function (tweet) {
   let $tweet = $(`
-  <br>
-  <article class="tweet">
-  <header class="tweet-header">
-  <img class="tweet-avatar" src="${tweet.user.avatars}" />
-  <span class="tweet-username">${tweet.user.name}</span>
-  <span class="tweet-handle hover-state">${tweet.user.handle}</span>
-  </header>
-  <h3>
-  ${tweet.content.text}
-  </h3>
-  <footer class="tweet-footer">
-  10 days ago
-  <img class="tweet-icons" src="/images/tweet-icons.jpg">
-  </footer>
-  </article> 
-  `);
+<br>
+<article class="tweet">
+<header class="tweet-header">
+<img class="tweet-avatar" src="${tweet.user.avatars}" />
+<span class="tweet-username">${tweet.user.name}</span>
+<span class="tweet-handle hover-state">${tweet.user.handle}</span>
+</header>
+<h3>
+${tweet.content.text}
+</h3>
+<footer class="tweet-footer">
+10 days ago
+<img class="tweet-icons" src="/images/tweet-icons.jpg">
+</footer>
+</article> 
+`);
 
   return $tweet;
 }
 
-renderTweets(data);
+// When Document is Ready ----------------------------------
+$(document).ready(() => {
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+
+  const $form = $('.tweet-submission');
+
+
+  $form.on('submit', function (event) {
+    const text = $(this).serialize();
+    event.preventDefault();
+
+    $.ajax({
+      type: "POST",
+      url: '/tweets',
+      data: text
+    }).then(function (data) {
+      console.log(data);
+    }).catch(function (data) {
+      console.log('Error: ', data);
+    });
+  });
+
+  const loadTweets = function() {
+    $.ajax({
+      type: "GET",
+      url: '/tweets'
+    }).then(function (data) {
+      renderTweets(data);
+      const $existingTweet = $('.tweet');
+      $existingTweet.on('mouseenter', function () {
+        const handle = $(this).parent().find('.tweet-handle');
+        handle.removeClass('hover-state');
+        $existingTweet.addClass('box-shadow');
+      });
+      
+      $existingTweet.on('mouseleave', function() {
+        const handle = $(this).parent().find('.tweet-handle');
+        handle.addClass('hover-state');
+        $existingTweet.removeClass('box-shadow')
+      });
+    })
+
+  }
+
+  loadTweets();
 
 });
+
+// $(function() {
+//   const $button = $('#load-more-posts');
+//   $button.on('click', function () {
+//     console.log('Button clicked, performing ajax call...');
+//     $.ajax('more-posts.html', { method: 'GET' })
+//     .then(function (morePostsHtml) {
+//       console.log('Success: ', morePostsHtml);
+//       $button.replaceWith(morePostsHtml);
+//     });
+//   });
+// });
