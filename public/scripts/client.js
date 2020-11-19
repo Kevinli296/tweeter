c7950f3104a7ae19151d1c4056cbf0fe1aefac2d
Kevinli296 <kevinli296@outlook.com>
2020-11-19 21:40:32 +0000

    Modified code to prevent cross site scripting, now protected!

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,41 +10,48 @@ const $textArea = $('#tweet-text');
 const $tweetsContainer = $('#tweets-container');
 
 // FUNCTIONS ----------------------------------------------
-const renderTweets = function (tweets) {
+const renderTweets = function(tweets) {
   let result = {};
-  $('#tweets-container').empty();
+  $tweetsContainer.empty();
   for (const tweet of tweets) {
     result = createTweetElement(tweet);
-    $('#tweets-container').prepend(result);
+    $tweetsContainer.prepend(result);
   }
 
   return result;
-}
+};
 
-const createTweetElement = function (tweet) {
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const createTweetElement = function(tweet) {
+  const safeHTML = `${escape(tweet.content.text)}`;
   let $tweet = $(`
-<br>
-<article class="tweet">
-<header class="tweet-header">
-<img class="tweet-avatar" src="${tweet.user.avatars}" />
-<span class="tweet-username">${tweet.user.name}</span>
-<span class="tweet-handle">${tweet.user.handle}</span>
-</header>
-<h3>
-${tweet.content.text}
-</h3>
-<footer class="tweet-footer">
-10 days ago
-<img class="tweet-icons" src="/images/tweet-icons.jpg">
-</footer>
-</article> 
-`);
+  <br>
+  <article class="tweet">
+  <header class="tweet-header">
+  <img class="tweet-avatar" src="${tweet.user.avatars}" />
+  <span class="tweet-username">${tweet.user.name}</span>
+  <span class="tweet-handle">${tweet.user.handle}</span>
+  </header>
+  <h3>
+  ${safeHTML}
+  </h3>
+  <footer class="tweet-footer">
+  10 days ago
+  <img class="tweet-icons" src="/images/tweet-icons.jpg">
+  </footer>
+  </article> 
+  `);
 
   return $tweet;
-}
+};
 
-const tweetSubmit = function () {
-  $form.on('submit', function (event) {
+const tweetSubmit = function() {
+  $form.on('submit', function(event) {
     const text = $(this).serialize();
     event.preventDefault();
 
@@ -59,25 +66,25 @@ const tweetSubmit = function () {
         type: "POST",
         url: '/tweets',
         data: text
-      }).then(function (data) {
+      }).then(function(data) {
         $form.trigger('reset');
         loadTweets();
         console.log('Success: ', data);
-      }).catch(function (data) {
+      }).catch(function(data) {
         console.log('Error: ', data);
       });
     }
   });
-}
+};
 
-const loadTweets = function () {
+const loadTweets = function() {
   $.ajax({
     type: "GET",
     url: '/tweets'
-  }).then(function (data) {
+  }).then(function(data) {
     renderTweets(data);
   });
-}
+};
 
 // When Document is Ready ----------------------------------
 $(document).ready(() => {
