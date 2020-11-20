42d09e47cce6b0e9b504f47add8971536df233b5
Kevinli296 <kevinli296@outlook.com>
2020-11-20 03:52:53 +0000

    Cleaned up code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,20 @@
-$(document).ready(() => {
-
-  const $tweetText = $('#tweet-text');
+const $tweetText = $('#tweet-text');
   
+const charCounter = function() {
   $tweetText.on('input', function() {
-    const value = 140 - $(this).val().length;
-    const counter = $(this).parent().find('#counterNumber');
-    counter.val(value);
-    if (value < 0) {
-      counter.addClass('red-color');
-    } else {
-      counter.removeClass('red-color');
-    }
-    
-  });
+  const value = 140 - $(this).val().length;
+  const counter = $(this).parent().find('#counterNumber');
+  counter.val(value);
+  if (value < 0) {
+    counter.addClass('red-color');
+  } else {
+    counter.removeClass('red-color');
+  }
+  
+});
+
+}
+
+$(document).ready(() => {
+  charCounter();
 });
